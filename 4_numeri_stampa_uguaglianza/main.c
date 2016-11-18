2db365a4c6ae382121c3b79aa115639c583e51c1
Vladimir <barabash97@gmail.com>
2016-11-18 15:21:33 +0100

    Scrivi un programma che chiede all'utente di inserire quattro numeri interi, li legge e stampa uno dei seguenti messaggi:
    - "Ci sono esattamente 4 numeri fra loro uguali" (se i numeri sono ad esempio 1,1,1,1)
    
    - "Ci sono esattamente 3 numeri fra loro uguali" (se i numeri sono ad esempio 1,1,1,2)
    
    - "Ci sono esattamente 2 numeri fra loro uguali" (se i numeri sono ad esempio 1,1,2,3)
    
    - "Ci sono due coppie di numeri fra loro uguali" (se i numeri sono ad esempio 1,1,2,2)
    
    - "Ci sono esattamente 0 numeri fra loro uguali" (se i numeri sono ad esempio 1,2,3,4)
    
    Parte NON ESSENZIALE, ma riservata solo ai più curiosi (una soluzione verrà pubblicata online quando almeno uno studente avrà inviato una soluzione ottima a frati@dia.uniroma3.it). Come si risolve l'esercizio potendo utilizzare solamente istruzioni di stampa e lettura, l' operatore di assegnamento, operatori matematici (+, -, *, /), operatori di confronto (==, >, ...), ed UNA SOLA ISTRUZIONE IF-ELSE? Sono quindi vietate istruzioni ripetitive, istruzioni condizionali diverse da if-else, array, stringhe, OPERATORI LOGICI (||, &&, !) ...

--- 4_numeri_stampa_uguaglianza/main.c
+++ 4_numeri_stampa_uguaglianza/main.c
@@ -1,32 +1,42 @@
+/* 
+ * Scrivi un programma che chiede all'utente di inserire quattro numeri interi, li legge e stampa uno dei seguenti messaggi:
+
+- "Ci sono esattamente 4 numeri fra loro uguali" (se i numeri sono ad esempio 1,1,1,1)
+
+- "Ci sono esattamente 3 numeri fra loro uguali" (se i numeri sono ad esempio 1,1,1,2)
+
+- "Ci sono esattamente 2 numeri fra loro uguali" (se i numeri sono ad esempio 1,1,2,3)
+
+- "Ci sono due coppie di numeri fra loro uguali" (se i numeri sono ad esempio 1,1,2,2)
+
+- "Ci sono esattamente 0 numeri fra loro uguali" (se i numeri sono ad esempio 1,2,3,4)
+
+Parte NON ESSENZIALE, ma riservata solo ai più curiosi (una soluzione verrà pubblicata online quando almeno uno studente avrà inviato una soluzione ottima a frati@dia.uniroma3.it). Come si risolve l'esercizio potendo utilizzare solamente istruzioni di stampa e lettura, l' operatore di assegnamento, operatori matematici (+, -, *, /), operatori di confronto (==, >, ...), ed UNA SOLA ISTRUZIONE IF-ELSE? Sono quindi vietate istruzioni ripetitive, istruzioni condizionali diverse da if-else, array, stringhe, OPERATORI LOGICI (||, &&, !) ... 
+*/
+
+
+/* GITHUB: https://github.com/barabash97/roma3_c 
+ * E-MAIL: vol.barabash@stud.uniroma3.it */
 #include <stdio.h>
 
 int main(int argc, char **argv)
 {
-	int a, b, c, d;
+	
+    int a,b,c,d;
     
     int i = 0;
+
+    printf("Caro utente, inserisci i numeri a,b,c,d.\n");
+    scanf("%d%d%d%d", &a,&b,&c,&d);
     
-    printf("Inserisci quattro numeri interi:\n");
+    if(((a==b) + (c==d) + (b!=c) + (a != d)) == 4) {
+        printf("Ci sono due coppie dEi numeri fra loro uguali.\n"); //OK
+    } else {
+        i += ( (a==b) + (b == a) + (b==c) + (c==d) + (a==d));
+        
+        i-= (a==d);
+        printf("Ci sono %d numeri fra loro uguali.\n", i);
+    }
     
-    /* Input */
-    printf("Primo numero:\t");
-    scanf("%d", &a);
-    
-    printf("Secondo numero:\t");
-    scanf("%d", &b);
-    
-    printf("Terzo numero:\t");
-    scanf("%d", &c);
-    
-    printf("Quarto numero:\t");
-    scanf("%d", &d);
-    
-    /* Processing */
-    
-    i+=((a==b) + ((b==c) - (b == a)) + (c==d) + (d==a));
-    
-    /* Output */
-    
-    printf("\n\nCi sono esattamente %d numeri fra loro uguali.\n\n", i);
 	return 0;
 }
