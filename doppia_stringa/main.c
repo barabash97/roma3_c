9b204f174efd1ac3659340df47c9528743cef14b
Volodymyr Barabash <barabash97@gmail.com>
2017-04-10 15:24:20 +0200

    Creato un nuovo workspace per il secondo esonero di informatica. Creato algoritmo doppiaStringa();

---
+++ doppia_stringa/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define DIM 50
+
+int doppiaStringa(char *stringa);
+
+int main(int argc, char **argv)
+{
+    /* Input */
+	char stringa[DIM];
+    
+    
+    printf("Caro utente,\ninserisci una frase.\n");
+    printf("Frase:\t");
+    fgets(stringa, DIM, stdin);
+    
+    if(doppiaStringa(stringa)){
+        printf("Si e\' verificata una doppia stringa.\n");
+    } else {
+        printf("Non si e\' verificata una doppia stringa.\n");
+
+    }
+    
+	return 0;
+}
+
+/* Esistenza universale */
+int doppiaStringa(char *stringa){
+    int flag = 1;
+    int i, j;
+    strtok(stringa, "\n");
+    int lunghezza = strlen(stringa);
+    j = (lunghezza / 2);
+    for(i = 0; i < (lunghezza / 2) && flag; i++){
+        if(stringa[i] != stringa[j]) flag = 0;
+        j++;
+    }
+    
+    return flag;
+}
